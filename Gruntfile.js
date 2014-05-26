66b1bf240be743a55218e3ba5691ce92a977264d
David Neubauer <davidneub@gmail.com>
2014-05-26 02:04:59 +0200

    modified gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -292,7 +292,7 @@ module.exports = function (grunt) {
       dist: {
         options: {
           remote: 'git@github.com:manymes/manymes.github.io.git',
-          branch: 'gh-pages',
+          branch: 'master',
           commit: true,
           push: true
         }
